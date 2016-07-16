e860b66dabc20d4c92868326a036b0aab9608975
Makss24 <maximefreland@gmail.com>
2016-07-17 01:09:42 +0200

    add field to insert new entry into MongoDB

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -3,8 +3,32 @@ import { Tasks } from '../api/tasks.js';
  
 import './body.html';
  
+// Ajout des Helpers == Data context pour Blaze
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    // Show newest tasks at the top
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+// Ajout des évenements par l'appel de la mathode 'events' sur le template 'body'
+Template.body.events({
+  // 'event selecteur'
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+ 
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+ 
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+ 
+    // Clear form
+    target.text.value = '';
   },
 });
